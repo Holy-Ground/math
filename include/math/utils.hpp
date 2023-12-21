d6db3a23bd81b92085107937ea609403c7b7f758
movingChurch <seong9115@gmail.com>
2023-12-21 15:07:55 +0900

    Added skeleton codes for math utils

---
+++ include/math/utils.hpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 HolyGround, All Rights Reserved.
+// Authors: EunSung Yang
+
+#ifndef HOLYGROUND__MATH__UTILS_HPP_
+#define HOLYGROUND__MATH__UTILS_HPP_
+
+namespace holyground::math {}  // namespace holyground::math
+
+#endif  // HOLYGROUND__MATH__UTILS_HPP_
---
+++ src/utils.cpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2023 HolyGround, All Rights Reserved.
+// Authors: EunSung Yang
+
+#include "math/utils.hpp"
+
